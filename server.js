a0c5e264b3eae84d0d30da179ba1c7932b2a7fd3
Jyadav816 <jyadav2@uncc.edu>
2021-11-24 23:25:16 -0500

    updated the server.js file

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const databaseConeection = require('./database')
 app.use(express.json())
 app.use('/api/cars/' , require('./paths/carsPath'))
 app.use('/api/users/' , require('./paths/uPath'))
+app.use('/api/bookings/' , require('./paths/bookPath'))
 
 app.get('/', (req,res) => res.send('hello world'))
 app.listen(port, () => console.log('Node js server started in port', port))
